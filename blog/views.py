cab5b560dde579f86e263b3736e57685ba7022be
cipriano macuja <knoxmacuja@gmail.com>
2022-11-03 14:47:06 +0000

    Finish First-Party Django REST API

--- blog/views.py
+++ blog/views.py
@@ -10,27 +10,27 @@ def post_detail(request, slug):
     return render(request, "blog/post-detail.html", {"post":
 post})
 
-if request.user.is_active:
-    if request.method == "POST":
-        comment_form = CommentForm(request.POST)
+    if request.user.is_active:
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
 
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.content_object = post
-            comment.creator = request.user
-            comment.save()
-        return redirect(request.path_info)
-    else:
-        comment_form = CommentForm()
-else:
-  comment_form = None
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.content_object = post
+                comment.creator = request.user
+                comment.save()
+                return redirect(request.path_info)
+            else:
+                comment_form = CommentForm()
+        else:
+            comment_form = None
 
 def index(request):
     posts = Post.objects.filter(published_at__lte=timezone.now())
     return render(request, "blog/index.html", {"posts": posts})
             
     
-return render(
-    request, "blog/post-detail.html", {"post": post,
-      "comment_form": comment_form}
-)
+    return render(
+        request, "blog/post-detail.html", {"post": post,
+        "comment_form": comment_form}
+    )
